d81919fadfcbfc7e44030ce86c2068ed29c95ebe
joshhughes <stalak@gmail.com>
2022-12-01 22:18:14 +0000

    Day 1

---
+++ AdventOfCode2022/Day1.cs
@@ -0,0 +1,32 @@
+﻿namespace AdventOfCode2022;
+
+public static class Day1
+{
+    public static void Run()
+    {
+        // https://adventofcode.com/2022/day/1
+        var calorieEntries = File.ReadAllLines("Day1Input.txt");
+
+        List<int> totalCaloriesPerElf = new();
+        var elfTotal = 0;
+
+        foreach (var calorieEntry in calorieEntries)
+        {
+            if (string.IsNullOrEmpty(calorieEntry))
+            {
+                totalCaloriesPerElf.Add(elfTotal);
+                elfTotal = 0;
+            }
+            else
+            {
+                elfTotal += int.Parse(calorieEntry);
+            }
+        }
+
+        Console.WriteLine($"Part 1: The elf with the most calories is carrying {totalCaloriesPerElf.Max()} calories");
+
+        var topThreeTotal = totalCaloriesPerElf.OrderBy(x => x).TakeLast(3).Sum();
+
+        Console.WriteLine($"\nPart 2: The top three elves carrying the most calories are carrying a total of {topThreeTotal}");
+    }
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,10 @@
+﻿using AdventOfCode2022;
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Press enter to run Day 1");
+Console.ReadLine();
+Console.ForegroundColor = ConsoleColor.White;
+
+Day1.Run();
+
+Console.ReadLine();
